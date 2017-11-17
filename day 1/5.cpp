4a288162979260c07e54457f1e7e28540c28917c
Klymenko69 <Klymenko696@gmail.com>
2017-11-17 08:27:26 +0200

    Added day1 again

---
+++ day 1/5.cpp
@@ -0,0 +1,19 @@
+/*Після введення з клавіатури довільного рядка 
+визначити і вивести на екран 
+кількість латинських літер у ньому.*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+ 
+using namespace std;
+ 
+bool f(int i) {
+    return (isalpha(i));
+}
+int main() {
+    string str;
+    getline(cin,str);
+    cout<<count_if(str.begin(),str.end(), f);
+        system("pause");
+    return 0;
+}
